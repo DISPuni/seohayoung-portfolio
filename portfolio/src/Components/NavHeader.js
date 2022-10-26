b3caa3d9a9e6de625cfea77a62fed4509441d234
Sugyeong <onmay16@gmail.com>
2022-10-25 22:44:56 -0500

    Footer/Nav

---
+++ portfolio/src/Components/NavHeader.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import './NavHeader.css'
+
+import logo from '../assets/icons/disp_mainlogo.svg'
+
+function NavHeader(props) {
+
+    let navigate = useNavigate();
+    function toHome() {
+        navigate('/home')
+    }
+
+    return (
+        <div id='navHeader'>
+            <img id='headerLogo' src={logo} alt="main logo" onClick={() => toHome()}/>
+            <span id='nav' style={{ display: props.isNav ? 'flex':'none'}}>
+                <span id='navAbout' style={{ color: props.isAbout ? '#7E7E86':'#161619' }}>ABOUT</span>
+                <span id='navBlog' style={{ color: props.isBlog ? '#7E7E86':'#161619' }}>BROG</span>
+            </span>
+        </div>
+    )
+}
+
+export default NavHeader
