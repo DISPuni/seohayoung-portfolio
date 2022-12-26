6fa2fd78338c2ac33e89944584bb63299e6d050c
DISPuni <111435077+DISPuni@users.noreply.github.com>
2022-12-27 03:52:33 +0900

    Update MediaPost3.js

--- portfolio/src/Pages/ServePost/MediaContent/MediaPost3.js
+++ portfolio/src/Pages/ServePost/MediaContent/MediaPost3.js
@@ -1,30 +1,23 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 import '../ServePost.css'
 
-import souvenir0 from '../../../assets/media/motion_souvenir/souvenir_000.mp4'
 import souvenir1 from '../../../assets/media/motion_souvenir/souvenir_001.png'
 import souvenir2 from '../../../assets/media/motion_souvenir/souvenir_002.png'
 
 function MediaPost3() {
 
-    const videoRef = useRef(null);
-
-    function videoPause() {
-        if (videoRef.current.paused) {
-            console.log('puaused');
-            videoRef.current.play();
-        } else {
-            console.log('played');
-            videoRef.current.pause();
-        }
-    }
-
     return (
         <div className='serve-content-body'>
-            <video className='content-video' controls autoPlay ref={videoRef} onClick={() => videoPause()}>
-                <source src={souvenir0} type="video/mp4" />
-            </video>
+            <div className='content-video'>
+                <iframe
+                    className='responsive-iframe'
+                    src={`https://www.youtube.com/embed/u1gb4znNeRE`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
             <img className='content-img' src={souvenir1} alt="" />
             <img className='content-img' src={souvenir2} alt="" />
         </div>
