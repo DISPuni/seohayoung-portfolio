f76b9dee3232929f054ab2a9ae5493bf873d48f2
DISPuni <111435077+DISPuni@users.noreply.github.com>
2022-12-27 03:51:39 +0900

    Update MediaPost2.js

--- portfolio/src/Pages/ServePost/MediaContent/MediaPost2.js
+++ portfolio/src/Pages/ServePost/MediaContent/MediaPost2.js
@@ -1,29 +1,22 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 import '../ServePost.css'
 
-import shimshim0 from '../../../assets/media/interaction_shimshim/shimshim_000.mp4'
 import shimshim1 from '../../../assets/media/interaction_shimshim/shimshim_001.png'
 
 function MediaPost2() {
 
-    const videoRef = useRef(null);
-
-    function videoPause() {
-        if (videoRef.current.paused) {
-            console.log('puaused');
-            videoRef.current.play();
-        } else {
-            console.log('played');
-            videoRef.current.pause();
-        }
-    }
-
     return (
         <div className='serve-content-body'>
-            <video className='content-video' controls autoPlay ref={videoRef} onClick={() => videoPause()}>
-                <source src={shimshim0} type="video/mp4" />
-            </video>
+            <div className='content-video'>
+                <iframe
+                    className='responsive-iframe'
+                    src={`https://www.youtube.com/embed/HTiNnOj-rPk`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
             <img className='content-img' src={shimshim1} alt="" />
         </div>
     )
