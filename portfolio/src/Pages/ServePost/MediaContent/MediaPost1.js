d838d7be762c371918c47328041c463c73da023a
DISPuni <111435077+DISPuni@users.noreply.github.com>
2022-12-27 03:51:20 +0900

    Update MediaPost1.js

--- portfolio/src/Pages/ServePost/MediaContent/MediaPost1.js
+++ portfolio/src/Pages/ServePost/MediaContent/MediaPost1.js
@@ -1,30 +1,23 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 import '../ServePost.css'
 
-import pop0 from '../../../assets/media/animation_pop/pop_000.mp4'
 import pop1 from '../../../assets/media/animation_pop/pop_001.png'
 import pop2 from '../../../assets/media/animation_pop/pop_002.png'
 
 function MediaPost1() {
 
-    const videoRef = useRef(null);
-
-    function videoPause() {
-        if (videoRef.current.paused) {
-            console.log('puaused');
-            videoRef.current.play();
-        } else {
-            console.log('played');
-            videoRef.current.pause();
-        }
-    }
-
     return (
         <div className='serve-content-body'>
-            <video className='content-video' controls autoPlay ref={videoRef} onClick={() => videoPause()}>
-                <source src={pop0} type="video/mp4" />
-            </video>
+            <div className='content-video'>
+                <iframe
+                    className='responsive-iframe'
+                    src={`https://www.youtube.com/embed/-lkTPa8tlJ4`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
             <img className='content-img' src={pop1} alt="" />
             <img className='content-img' src={pop2} alt="" />
         </div>
