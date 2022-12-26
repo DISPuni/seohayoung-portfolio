8a822f6f8ed9517793e6be6e4161db53dcf6b88f
DISPuni <111435077+DISPuni@users.noreply.github.com>
2022-12-27 03:53:07 +0900

    Update MediaPost5.js

--- portfolio/src/Pages/ServePost/MediaContent/MediaPost5.js
+++ portfolio/src/Pages/ServePost/MediaContent/MediaPost5.js
@@ -1,30 +1,23 @@
-import React, { useRef } from 'react'
+import React from 'react'
 
 import '../ServePost.css'
 
-import todaywho0 from '../../../assets/media/uiux_todaywho/todaywho_000.mp4'
 import todaywho1 from '../../../assets/media/uiux_todaywho/todaywho_001.png'
 import todaywho2 from '../../../assets/media/uiux_todaywho/todaywho_002.png'
 
 function MediaPost5() {
 
-    const videoRef = useRef(null);
-
-    function videoPause() {
-        if (videoRef.current.paused) {
-            console.log('puaused');
-            videoRef.current.play();
-        } else {
-            console.log('played');
-            videoRef.current.pause();
-        }
-    }
-
     return (
         <div className='serve-content-body'>
-            <video className='content-video' controls autoPlay ref={videoRef} onClick={() => videoPause()}>
-                <source src={todaywho0} type="video/mp4" />
-            </video>
+            <div className='content-video'>
+                <iframe
+                    className='responsive-iframe'
+                    src={`https://www.youtube.com/embed/_gfihwT4RTs`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
             <img className='content-img' src={todaywho1} alt="" />
             <img className='content-img' src={todaywho2} alt="" />
         </div>
