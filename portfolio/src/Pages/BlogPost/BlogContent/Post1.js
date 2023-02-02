b862abf34299c0753f3b728ce8980c3a96385490
DISPuni <111435077+DISPuni@users.noreply.github.com>
2023-02-02 23:48:41 +0900

    Update Post1.js

--- portfolio/src/Pages/BlogPost/BlogContent/Post1.js
+++ portfolio/src/Pages/BlogPost/BlogContent/Post1.js
@@ -2,10 +2,42 @@ import React from 'react'
 
 import '../BlogPost.css'
 
+
 function Post1() {
-    return (
+        return (
         <div className='blog-content-body'>
-            Post1
+            <hr></hr>
+            <br></br>
+            <br></br>
+            <div className='blog_title'><b>프롤로그</b></div>
+            <div className='blog_subtitle'>포트폴리오 사이트</div>
+            <br></br>
+            <img className='blog_img' src='https://img1.daumcdn.net/thumb/R1280x0/?fname=http%3A//t1.daumcdn.net/brunch/service/user/dlxY/image/qgFhgvReVLx4hWk2rODL-NEaw3k.jpg'
+            width="100%"></img>
+            <br></br>
+            <br></br>
+            <div className='article'>
+            <b>“포트폴리오는 pdf면 그만이지 사이트를 굳이?” 라고 묻는이들도 많았다.</b>
+            <br></br>pdf는 그동안 걸어온 길을 너무나도 단편적인 면을 보여준다. 주니어 디자이너의 경우
+            <br></br>이러한 점을 보완하기 위해 개성이 너무많이 묻은 포트폴리오 편집을 보여준다던가, 
+            <br></br>많은 내용을 꾹꾹 눌러담은 눈아픈 작품 모음집을 만든다. 나도 마찬가지였다.
+            <br></br>
+            <br></br>기회가 닿아 타인의 포트폴리오 pdf를 읽어볼 기회가 생겼다. 이윽고 깨달았다
+            <br></br>남의 작품을 읽는건 생각보다 상당히, 귀찮은 일이라는것을.
+            <br></br>스크롤 하며 작품에 빼곡히 채워진 글은 한줄이상 읽기가 어렵다. 작품 사진만 보는데도 인지 과부화가 걸릴 지경이다.
+            <br></br>
+            <br></br>내가 만약 채용담당자라면 수많은 지원자의 작품을 스크롤하는 행위 자체가 꽤나 피로하겠다고 생각했다.
+            <br></br>이외에도, 커버부터 화려하면 작품을 보기도 전에 피곤해진다. 내용도 화려할것이 안봐도 보이니까..
+            <br></br>원하는 작품이 빨리 보이지 않으면, 중간에 창을 닫고 휴지통으로 버리기도 했다.
+            <br></br>
+            <br></br>지원자는 자신의 작품을 상대방이 꼼꼼이 정성껏 봐줄것이라 생각하고 화려하게, 최대한 많이 넣지만 
+            <br></br>상대방은 업무가 늘어나 모든게 피로할 뿐이다.. 
+            <br></br>
+            <br></br>그렇게 이날 밤새 사이트를 만들게 되었다.
+
+            </div>
+
+
         </div>
     )
 }
