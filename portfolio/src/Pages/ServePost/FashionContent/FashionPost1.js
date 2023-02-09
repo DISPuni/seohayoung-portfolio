79f798f619ec1b38ad2fe74f431b2269878d2c10
DISPuni <111435077+DISPuni@users.noreply.github.com>
2023-02-09 22:32:07 +0900

    Update FashionPost1.js

--- portfolio/src/Pages/ServePost/FashionContent/FashionPost1.js
+++ portfolio/src/Pages/ServePost/FashionContent/FashionPost1.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 import '../ServePost.css'
 
+import quench1 from '../../../assets/fashion/lookbook_quench/quench_001.png'
+import quench2 from '../../../assets/fashion/lookbook_quench/quench_002.png'
+
 function FashionPost1() {
     return (
         <div className='serve-content-body'>
-            FashionPost1
+            <img className='content-img' src={quench1} alt="" />
+            <img className='content-img' src={quench2} alt="" />
         </div>
     )
 }
