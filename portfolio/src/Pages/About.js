a0860b26f74d84757bbc91a32f4b387bf92138a1
DISPuni <111435077+DISPuni@users.noreply.github.com>
2023-02-20 14:57:29 +0900

    Update About.js

--- portfolio/src/Pages/About.js
+++ portfolio/src/Pages/About.js
@@ -28,6 +28,8 @@ import idLogo from '../assets/about/tools/indesign.svg'
 import paintLogo from '../assets/about/tools/paint.png'
 import procreateLogo from '../assets/about/tools/procreate.png'
 import figmaLogo from '../assets/about/tools/Figma.png'
+import protoLogo from '../assets/about/tools/proto.png'
+import zepLogo from '../assets/about/tools/zeplin.svg'
 import mayaLogo from '../assets/about/tools/maya.png'
 import c4dLogo from '../assets/about/tools/c4d.png'
 import iwLogo from '../assets/about/tools/imweb.png'
@@ -35,6 +37,11 @@ import wixLogo from '../assets/about/tools/wix.png'
 import htmlLogo from '../assets/about/tools/HTML5.png'
 import cssLogo from '../assets/about/tools/css.png'
 import jsLogo from '../assets/about/tools/js.png'
+import exLogo from '../assets/about/tools/excel.png'
+import wordLogo from '../assets/about/tools/word.png'
+import pptLogo from '../assets/about/tools/ppt.png'
+import teamsLogo from '../assets/about/tools/teams.png'
+
 
 function About() {
 
@@ -63,7 +70,7 @@ function About() {
                         <table>
                             <tr>
                                 <th>Made by</th>
-                                <td>DISP</td>
+                                <td><a href="https://www.instagram.com/disp_design/" target={'_blank'}>DISP</a></td>
                                 <td>SEOHAYOUNG</td>
                             </tr>
                             <tr>
@@ -73,7 +80,7 @@ function About() {
                             </tr>
                             <tr>
                                 <th>Contact</th>
-                                <td colSpan={2}>puni929@naver.com</td>
+                                <td colSpan={2}><a href="mailto:puni929@naver.com" target={'_blank'}>puni929@naver.com</a></td>
                             </tr>
                         </table>
                         <div id='aboutIconContainer' className='flex margin-auto'>
@@ -96,19 +103,27 @@ function About() {
                         <div id='aboutContainer' className='flex-col margin-auto'>
                             <div className='flex about-timeline' style={{ borderTop: '1px solid #43434B' }}>
                                 <div className='collapse-title'>Education</div>
-                                <Accordion years={['2020.03 ~ 2023.08 | 3Y', '2018.03 ~ 2020.02 | 2Y']} organizations={['Dounduk univ', 'Backseok art univ']} descriptions={[`Fashion Design<br/>Media Design`, 'Visual Design']} />
+                                <Accordion years={['2020.03 ~ 2023.08 | 3Y', '2018.03 ~ 2020.02 | 2Y']} 
+                                organizations={['Dounduk univ', 'Backseok art univ']} 
+                                descriptions={[`Fashion Design<br/>Media Design`, 'Visual Design']} />
                             </div>
                             <div className='flex about-timeline'>
                                 <div className='collapse-title'>Career</div>
-                                <Accordion years={['2021.12 ~ 2022.09 | 10M', '2021.07 ~ 2021.10 | 4M', '2020.12 ~ 2021.06 | 7M', '2020.04 ~ 2020.09 | 6M']} organizations={['CJ Oliveyoung', 'Amorepacific', 'UBIQUE KOREA', 'Hyundai AD']} descriptions={['Vcomerce design', 'Counseller Omni TF', 'Web design', 'design team Manager']} />
+                                <Accordion years={['2021.12 ~ 2022.09 | 10M', '2021.07 ~ 2021.10 | 4M', '2020.11 ~ 2021.07 | 9M', '2020.03 ~ 2020.11 | 8M']} 
+                                organizations={['CJ Oliveyoung', 'Amorepacific', 'UBIQUE KOREA', 'Hyundai AD']} 
+                                descriptions={['Media commerce team', 'Counselor Omni TF team', 'Web design', 'Design team Manager']} />
                             </div>
                             <div className='flex about-timeline'>
                                 <div className='collapse-title'>Project</div>
-                                <Accordion years={['2021.12 ~ 2022.12 | 1Y', '2022.08 ~ 2022.11 | 3M', '2022.05 ~ 2022.08 | 3M', '2022.07 ~ 2022.08 | 1M', '2022.03 ~ 2022.08 | 5M', '2021.07 ~ 2022.06 | 11M', '2021.04 ~ 2022.04 | 1Y']} organizations={['Exhibition WWW', 'Design Crew OOBB 2nd pyeonteilba', 'Fastcampus UIUX KDT company project', 'SPARTA bootcamp chang', 'SPARTA Design membership', 'Design Crew OOBB 1st hotel shimshim', 'Student Fashion Association OFF']} descriptions={['Diractor', 'PM & Editorial design', 'OOBB Founder & team leader', 'UIUX design', '1st team', 'OOBB Founder & team leader', '27th Planner team']} />
+                                <Accordion years={['2023.02 ~ 2024.02 | 1Y', '2021.12 ~ 2022.12 | 1Y', '2022.05 ~ 2022.12 | 7M', '2022.05 ~ 2022.08 | 3M', '2022.07 ~ 2022.08 | 1M', '2022.03 ~ 2022.08 | 5M', '2021.07 ~ 2022.06 | 11M', '2021.04 ~ 2022.04 | 1Y']} 
+                                organizations={['Osulloc tea creator', 'Exhibition WWW', 'Design Crew OOBB 2nd pyeonteilbar', 'Fastcampus UIUX KDT project', 'SPARTA bootcamp chang', 'SPARTA Design membership', 'Design Crew OOBB 1st hotel shimshim', 'Student Fashion Association OFF']} 
+                                descriptions={['creator team', 'Diractor', 'PM & Editorial design', 'UIUX team leader', 'UIUX design', '1st team', 'OOBB Founder & team leader', '27th Planner team']} />
                             </div>
                             <div className='flex about-timeline'>
                                 <div className='collapse-title'>Award</div>
-                                <Accordion years={['2021.12.31', '2021.12.06', '2021.07.30']} organizations={['Dedign for the future', 'it award challenge', 'Communication design association of korea']} descriptions={['Special Prize autodesk prize', 'Bronze frize', 'Special prize']} />
+                                <Accordion years={['2023.2.10','2021.12.31', '2021.12.06', '2021.07.30']} 
+                                organizations={['Asia design award','Design for the future', 'it award challenge', 'Communication design association of korea']} 
+                                descriptions={['Finalist','Special Prize autodesk Prize', 'Bronze Prize', 'Special Prize']} />
                             </div>
                         </div>
                     </div>
@@ -124,6 +139,8 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={paintLogo} alt="paint logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={procreateLogo} alt="procreate logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={figmaLogo} alt="figma logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={protoLogo} alt="proto logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={zepLogo} alt="zeplin logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={mayaLogo} alt="maya logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={c4dLogo} alt="c4d logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={iwLogo} alt="iw logo" /></li>
@@ -131,6 +148,10 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={htmlLogo} alt="html logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={cssLogo} alt="css logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={jsLogo} alt="js logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={exLogo} alt="ex logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={wordLogo} alt="word logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={pptLogo} alt="ppt logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={teamsLogo} alt="teams logo" /></li>
                         </ul>
                         <ul className="about-marquee-content flex" aria-hidden='true'>
                             <li className='about-marquee-item'><div id='availableToolText'>Available Tools</div></li>
@@ -143,6 +164,8 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={paintLogo} alt="paint logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={procreateLogo} alt="procreate logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={figmaLogo} alt="figma logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={protoLogo} alt="proto logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={zepLogo} alt="zeplin logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={mayaLogo} alt="maya logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={c4dLogo} alt="c4d logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={iwLogo} alt="iw logo" /></li>
@@ -150,6 +173,10 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={htmlLogo} alt="html logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={cssLogo} alt="css logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={jsLogo} alt="js logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={exLogo} alt="ex logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={wordLogo} alt="word logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={pptLogo} alt="ppt logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={teamsLogo} alt="teams logo" /></li>
                         </ul>
                         <ul className="about-marquee-content flex" aria-hidden='true'>
                             <li className='about-marquee-item'><div id='availableToolText'>Available Tools</div></li>
@@ -162,6 +189,8 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={paintLogo} alt="paint logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={procreateLogo} alt="procreate logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={figmaLogo} alt="figma logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={protoLogo} alt="proto logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={zepLogo} alt="zeplin logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={mayaLogo} alt="maya logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={c4dLogo} alt="c4d logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={iwLogo} alt="iw logo" /></li>
@@ -169,6 +198,10 @@ function About() {
                             <li className='about-marquee-item'><img className='tool' src={htmlLogo} alt="html logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={cssLogo} alt="css logo" /></li>
                             <li className='about-marquee-item'><img className='tool' src={jsLogo} alt="js logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={exLogo} alt="ex logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={wordLogo} alt="word logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={pptLogo} alt="ppt logo" /></li>
+                            <li className='about-marquee-item'><img className='tool' src={teamsLogo} alt="teams logo" /></li>
                         </ul>
                     </div>
                     <SecondFooter isAbout={true}/>
