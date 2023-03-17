3da429a9f2136a196a9ebe9667969ff0f33ede11
DISPuni <111435077+DISPuni@users.noreply.github.com>
2023-03-17 23:49:07 +0900

    Update BlogPostHeader.js

--- portfolio/src/Pages/BlogPost/BlogPostHeader.js
+++ portfolio/src/Pages/BlogPost/BlogPostHeader.js
@@ -14,6 +14,8 @@ import blog from '../../assets/icons/blog.svg'
 
 import Post1 from './BlogContent/Post1'
 import Post2 from './BlogContent/Post2'
+import Post3 from './BlogContent/Post3'
+import Post4 from './BlogContent/Post4'
 
 function BlogPostTemplate() {
 
@@ -22,6 +24,8 @@ function BlogPostTemplate() {
     const postTitle = [
         { id:1, title:'#1. DISP Site history' , subtitle:'사이트의 존재 이유' },
         { id:2, title:'#2. 쓰지않는 금연어플들' , subtitle:'고쳐서 정리하기' },
+        { id:3, title:'#3. 플러스엑스 UIUX 강의 후기' , subtitle:'생각정리 겸, 후기 이벤트 겸..' },
+        { id:4, title:'#4. 디자인과 인공지능' , subtitle:'인공지능을 이용하는 방법' },
     ]
     const [post, setPost] = useState()
 
@@ -30,6 +34,10 @@ function BlogPostTemplate() {
     function toBlog() {
         navigate('/blog')
     }
+    
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
 
     useEffect(() => {
         if (postId.id === '1') {
@@ -37,6 +45,12 @@ function BlogPostTemplate() {
         }
         else if (postId.id === '2') {
             setPost(<Post2 />)
+        }
+        else if (postId.id === '3') {
+            setPost(<Post3 />)
+        }
+        else if (postId.id === '4') {
+            setPost(<Post4 />)
         }
     }, [])
     
@@ -49,6 +63,7 @@ function BlogPostTemplate() {
             <div className='blog-post-body'>
                 <TopBtn />
                 <div className='back-btn'><div onClick={toBlog}>Back</div></div>
+                <div className='serve-filter'>Blog</div>
                 <img className='blog-post-logo' src={blog} alt="" />
                 <div className='blog-post-title'>{postTitle[postId.id-1].title}</div>
                 <div className='blog-post-subtitle'>{postTitle[postId.id-1].subtitle}</div>
